b8b41836ff36a585b75267df3eaa1bae217b0e74
chukkydave <chukkydave@gmail.com>
2021-03-06 14:39:07 +0100

    refactored the fetchData func and movies/movie vars

--- autocomplete.js
+++ autocomplete.js
@@ -1,6 +1,6 @@
-const createAutoComplete = ({ root, renderOption, onOptionSelect, inputValue }) => {
+const createAutoComplete = ({ root, renderOption, onOptionSelect, inputValue, fetchData }) => {
 	root.innerHTML = `
-    <label><b>Search for a Movie</b></label>
+    <label><b>Search</b></label>
     <input class="input" type="text">
     <div class="dropdown">
       <div class="dropdown-menu">
@@ -15,23 +15,23 @@ const createAutoComplete = ({ root, renderOption, onOptionSelect, inputValue }) 
 	const resultsWrapper = root.querySelector('.results');
 
 	const onInput = async (event) => {
-		const movies = await fetchData(event.target.value);
-		if (!movies.length) {
+		const items = await fetchData(event.target.value);
+		if (!items.length) {
 			dropdown.classList.remove('is-active');
 			return;
 		}
 
 		resultsWrapper.innerHTML = '';
 		dropdown.classList.add('is-active');
-		for (let movie of movies) {
+		for (let item of items) {
 			let option = document.createElement('a');
 
 			option.classList.add('dropdown-item');
-			option.innerHTML = renderOption(movie);
+			option.innerHTML = renderOption(item);
 			option.addEventListener('click', () => {
-				input.value = inputValue(movie);
+				input.value = inputValue(item);
 				dropdown.classList.remove('is-active');
-				onOptionSelect(movie);
+				onOptionSelect(item);
 			});
 			resultsWrapper.appendChild(option);
 		}
